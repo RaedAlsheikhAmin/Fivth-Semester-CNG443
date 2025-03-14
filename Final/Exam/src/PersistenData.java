d19676527ee949548cb56d264e69367161365f1a
Raed Alsheikh Amin <raed.alshekh.amin@gmail.com>
2025-03-15 02:18:47 +0300

    untracking useless files

--- Final/Exam/src/PersistenData.java
+++
@@ -1,7 +0,0 @@
-public interface PersistenData {
-    public  void readStudent(Exam bnb);
-    public void writeStudent(Exam bnb);
-    public void readInternship(Exam bnb);
-    public void writeInternship(Exam bnb);
-
-}
